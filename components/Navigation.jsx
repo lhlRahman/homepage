d0fcc94b51398fba161ad6a06ed428e14092d351
Abdul <lhlrahman@gmail.com>
2024-04-27 18:25:06 -0400

    Update Navigation component styling

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,20 +1,21 @@
 export const Navigation = () => {
     return (
-      <div className={`h-[290px] bg-[#D9D9D9] font-caudex text-[#373633] text-2xl`}>
-          <ul className="w-screen px-60 h-full flex justify-between items-center">
-              <li className="flex flex-row items-center md:flex-col">
-                  <span>01</span>
-                  <span>ABOUT US</span>
+      <div className={`bg-[#D9D9D9] font-caudex text-[#373633]`}>
+          <ul className="flex flex-col sm:flex-row sm:justify-between items-center sm:px-10 lg:px-60 py-8 text-xl lg:text-2xl space-y-8 sm:space-y-0">
+              <li className="flex flex-col items-center space-y-2">
+                  <h3>01</h3>
+                  <h3>ABOUT US</h3>
               </li>
-              <li className="flex flex-col items-center">
-                  <span>02</span>
-                  <span>CLIENT GALLERY</span>
+              <li className="flex flex-col items-center space-y-2">
+                  <h3>02</h3>
+                  <h3>CLIENT GALLERY</h3>
               </li>
-              <li className="flex flex-col items-center">
-                  <span>03</span>
-                 <h2>SERVICES</h2>
+              <li className="flex flex-col items-center space-y-2">
+                  <h3>03</h3>
+                  <h3>SERVICES</h3>
               </li>
           </ul>
       </div>
     )
   }
+  
