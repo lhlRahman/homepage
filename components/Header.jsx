926202539b26d5ac0187fea4129f902659de48c6
Abdul <lhlrahman@gmail.com>
2024-04-27 17:47:06 -0400

    Update button style in Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,33 +2,48 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const Header = () => {
+  const buttonStyle = {
+    width: '283px', // Assuming you want to use the width specified in the image
+    height: '66px',
+    backgroundColor: 'rgba(255, 255, 255, 0.75)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '75px',
+    border: '1px solid rgba(255, 255, 255, 0.5)',
+    boxShadow: '10px 10px 20px 0 rgba(69, 74, 124, 0.15), -10px -10px 20px 0 rgba(255, 255, 255, 0.3)',
+    fontSize: '16px',
+    lineHeight: '20px',
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+  };
+
   return (
     <header className="relative font-caudex w-screen h-[100vh]">
       <Image src="/images/background.png" layout="fill" objectFit="cover" />
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-center space-y-4 bg-black bg-opacity-50 px-4 sm:px-8 md:px-16 lg:px-32">
-        <h1 className="text-4xl font-bold text-white mx-auto text-center sm:text-right sm:mr-24" style={{
+      <div className="absolute top-0 right-0 w-full h-full flex flex-col justify-center items-end text-right space-y-4 bg-black bg-opacity-50 px-4 sm:px-8 md:px-16 lg:px-32">
+        <h1 className="text-4xl font-bold text-white mx-auto sm:text-5xl sm:mr-24" style={{
           fontWeight: '400',
           fontSize: '60px',
           lineHeight: "77.34px",
-        }}>Photography and
-        <br />
-        Videography Services</h1>
-        <p className="text-white text-center sm:text-right sm:mx-auto sm:mr-24"
-        style={{
+        }}>
+          Photography and
+          <br />
+          Videography Services
+        </h1>
+        <p className="text-white sm:mr-24" style={{
           fontFamily: 'Helvetica Neue',
-          weight: '400',
-          size: '25px',
+          fontWeight: '400',
+          fontSize: '25px',
           lineHeight: '30px',
         }}>
           We capture everything from graduation photos, music video occasions,
           <br />
           and intimate moments through lenses.
         </p>
-        <Link href="#" className="bg-white text-[#593419] px-6 py-4 rounded-3xl opacity-75 mx-auto sm:text-right sm:mr-24"
-        style={{
-          boxShadow: 'rgba(69, 42, 124, 0.15)'
-        }}>
-          <h3 className="opacity-100">BROWSE OUR SERVICES</h3>
+        <Link href="#" style={buttonStyle} className=" mr-24 mt-6">
+          <h3 style={buttonStyle}>BROWSE OUR SERVICES</h3>
         </Link>
       </div>
     </header>  
